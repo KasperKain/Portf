92ef00358343e8dc99ce342404d65933e5a11421
Kasper Kain <41598018+KasperKain@users.noreply.github.com>
2020-11-08 13:42:33 -0500

    Removed redundancy

--- script.js
+++ script.js
@@ -2,37 +2,27 @@ const main = document.querySelector("main");
 const sections = document.querySelectorAll("section");
 const section_buttons = document.getElementsByClassName("section-button")
 
-section_buttons[0].classList.add("selected");
-display_section(sections[0]);
+display_section(0);
 
 for (let i = 0; i < section_buttons.length; i++) {
   section_buttons[i].addEventListener('click', function () {
-    let section_size = document.querySelector("section").clientHeight;
-    main.scrollTop = (i * section_size);
 
     sections[i].classList.remove("play-anim");
     void sections[i].offsetWidth;
     sections[i].classList.add("play-anim");
 
-    button_click(section_buttons[i]);
-    display_section(sections[i]);
+    display_section(i);
   })
 }
 
 
-function display_section(section) {
+function display_section(index) {
   for (let i = 0; i < sections.length; i++) {
     sections[i].style.display = 'none';
-  }
-
-  section.style.display = 'flex';
-}
-
-function button_click(btn) {
-  for (let i = 0; i < section_buttons.length; i++) {
     section_buttons[i].classList.remove("selected");
   }
 
-  btn.classList.add("selected");
+  sections[index].style.display = 'flex';
+  section_buttons[index].classList.add("selected");
 }
 
